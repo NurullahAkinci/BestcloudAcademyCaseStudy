a351e30371a34bc5799ffdfc588e5618c8dc3e70
NurullahAkinci <77158327+NurullahAkinci@users.noreply.github.com>
2022-02-03 23:11:46 +0300

    API Edited

--- BestcloudAcademyCaseStudy/Controllers/TemperatureController.cs
+++ BestcloudAcademyCaseStudy/Controllers/TemperatureController.cs
@@ -12,20 +12,25 @@ namespace BestcloudAcademyCaseStudy.Controllers
     [Route("[controller]")]
     public class TemperatureController : ControllerBase
     {
-        [HttpGet]
-        public User Get()
-        {
-            User user = new User();
-            user.Firstname = "Nurullah";
-            user.Lastname = "Akıncı";
-            return user;
-        }
+        //[HttpGet]
+        //public User Get()
+        //{
+        //    User user = new User();
+        //    user.Firstname = "Nurullah";
+        //    user.Lastname = "Akıncı";
+        //    return user;
+        //}
 
-        [HttpGet("GetTemperature/{city}")]
-        public IActionResult GetTemperature([FromQuery] string city)
+        [HttpGet]
+        public IActionResult Get([FromQuery(Name = "city")] string city)
         {
             if (String.IsNullOrWhiteSpace(city))
-                return BadRequest("Bad Request. You must enter a city.");
+            {
+                User user = new User();
+                user.Firstname = "Nurullah";
+                user.Lastname = "Akıncı";
+                return Ok(user);
+            }
             WeatherForecast weather = new WeatherForecast();
             weather.Temperature = new Random().Next(-10, 20);
             return Ok(weather);
